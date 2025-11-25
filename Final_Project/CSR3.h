f6601baa625b8b2d892cf821276c1a60fc4d116f
Тимур Ильиных <55120919+m1Myp@users.noreply.github.com>
2025-11-25 22:05:21 +0700

    Cleaning CSR3.h

--- Final_Project/CSR3.h
+++ Final_Project/CSR3.h
@@ -7,16 +7,14 @@
 using namespace std;
 
 namespace Matrixes {
-	//zero-based
 	class CSR3 {
 		vector<double> values;
 		vector<int> columns;
 		vector<int> rowIndex;
 	public:
-		int M; // число строк
-		int N; //число столбцов
-		int L; //число ненулевых элементов
-		//Пусть i - номер колонки, j - номер строки
+		int M;
+		int N;
+		int L;
 		void setElement(int i, int j, double element);
 		double getElement(int i, int j);
 		CSR3 Submatrix(int from_i, int to_i, int from_j, int to_j);
@@ -27,7 +25,6 @@ namespace Matrixes {
 		static CSR3 Read(string filename);
 		void Write(string filename);
 
-		//Перегружаем операторы
 		friend CSR3 operator+(const CSR3& A, const CSR3& B);
 		CSR3 operator*(double scalar) const;
 		CSR3 operator-(CSR3& B) const;
