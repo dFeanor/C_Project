06de49d821040bc70d82e1decd417fe53f4be3b6
Тимур Ильиных <55120919+m1Myp@users.noreply.github.com>
2025-11-25 22:10:59 +0700

    Update Picture.h

--- Final_Project/Picture.h
+++ Final_Project/Picture.h
@@ -3,21 +3,13 @@
 
 #include <string>
 #include <vector>
-#include <cstdint> // <--- ВКЛЮЧАЕМ ДЛЯ ТИПОВ С ФИКСИРОВАННЫМ РАЗМЕРОМ
+#include <cstdint>
 #include <cstring>
 #include <math.h>
 using namespace std;
-// Определяем enum для указания оси среза. Это гораздо безопаснее, чем использовать
-// просто числа (0, 1, 2), так как защищает от передачи неверных значений.
 enum class SliceAxis {
-    // Срез перпендикулярно оси X (оси столбцов).
-    // Итоговое 2D изображение будет иметь размеры N1 (высота) x N3 (глубина).
     X,
-    // Срез перпендикулярно оси Y (оси строк).
-    // Итоговое 2D изображение будет иметь размеры N2 (ширина) x N3 (глубина).
     Y,
-    // Срез перпендикулярно оси Z (оси глубины).
-    // Итоговое 2D изображение будет иметь размеры N1 (высота) x N2 (ширина).
     Z
 };
 
@@ -31,13 +23,8 @@ public:
 
     bool saveToFile(const std::string& filePath) const;
 
-    // Принимает пустой объект Picture по ссылке для заполнения,
-    // координаты верхнего левого угла (startX, startY) и размер стороны квадрата (size).
     bool extractSubregion(Picture& outputPicture, uint64_t startX, uint64_t startY, uint64_t size) const;
-    //Mетод для извлечения 2D - слайса из 3D - изображения
     bool extractSlice(Picture& outputSlice, SliceAxis axis, uint64_t sliceIndex) const;
-
-    //uint64_t — это 64-битный (строго 8 байт) беззнаковый целочисленный тип данных в C++, который хранит целые числа
     uint64_t getDim1() const;
     uint64_t getDim2() const;
     uint64_t getDim3() const;
@@ -45,23 +32,13 @@ public:
     double getResolution() const;
     uint64_t getTotalPixels() const;
     const unsigned char* getPixelData() const;
-
-    // Метод для добавления стенок
     void addWalls();
-
-    //Вспомогательный метод для проверок и прочего
     void PrintPicture();
-
-    //Метод генерации цилиндра (библиотека стандартных изображений)
     void createCylinder(size_t width, size_t height, double radius);
-    //Метод генерации цилиндра с порой посередине
     void createCylinderWithPore(size_t width, size_t height, double radiusOuter, double radiusInner);
-    //Метод генерации извилистого канала - синусоиды (библиотека стандартных изображений)
     void createTortuousChannel(size_t width, size_t height, double channelWidth, double amplitude, double frequency);
-    //Метод удаления пор
     void removeIsolatedPores();
     private:
-    // Используем типы с гарантированным размером 8 байт
     uint64_t N1;
     uint64_t N2;
     uint64_t N3;
@@ -69,11 +46,10 @@ public:
     double resolution;
     unsigned char* pixels;
 
-    // Флаг, чтобы отслеживать, были ли уже добавлены стенки
     bool wallsAdded;
 
     Picture(const Picture&);
     Picture& operator=(const Picture&);
 };
 
-#endif // PICTURE_H
+#endif
